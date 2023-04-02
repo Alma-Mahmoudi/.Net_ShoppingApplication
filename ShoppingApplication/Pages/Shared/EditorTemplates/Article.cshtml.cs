baf08edd760a953ef3e5b5ca58dfc7391c4aa667
Alma-Mahmoudi <mahmoudialma96@gmail.com>
2023-04-02 18:57:00 +0200

    Add Modification

---
+++ ShoppingApplication/Pages/Shared/EditorTemplates/Article.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShoppingApplication.Pages.Shared.EditorTemplates
+{
+    public class ArticleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

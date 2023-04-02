874b2fe3c01deb4a33d0febf8fd2b9f4a202f6ed
Alma-Mahmoudi <mahmoudialma96@gmail.com>
2023-04-02 17:39:23 +0200

    Update Project

---
+++ ShoppingApplication/Options/PathOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingApplication.Options
+{
+    public class PathOptions
+    {
+        public const string Path = "Path";
+
+        public string ArticlesImages { get; set; } = string.Empty;
+    }
+}
---
+++ ShoppingApplication/Services/PathService.cs
@@ -0,0 +1,30 @@
+﻿using ShoppingApplication.Options;
+
+namespace ShoppingApplication.Services
+{
+    public class PathService
+    {
+        private readonly IConfiguration configuration;
+        private readonly IWebHostEnvironment env;
+        public PathService(IConfiguration configuration,IWebHostEnvironment env)
+        {
+            this.configuration = configuration;
+            this.env = env; 
+        }
+
+        public string GetUploadsPath(string filename=null,bool withWebRoothPath=true)
+        {
+            var pathOptions = new PathOptions();
+
+            configuration.GetSection(PathOptions.Path).Bind(pathOptions);
+
+            var uploadsPath = pathOptions.ArticlesImages;
+
+            if(null != filename)
+            {
+                uploadsPath = Path.Combine(uploadsPath, filename);
+            }
+            return withWebRoothPath ? Path.Combine(env.WebRootPath, uploadsPath) : uploadsPath;
+        }
+    }
+}
